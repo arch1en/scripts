62a270d3933d42b30df0d87e8ca03ac0ee7b69b2
arch1en <arch1en.gd@gmail.com>
2020-02-20 23:52:07 +0100

    [Server][Factorio] Improved factorio server script. Script is functional now.

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ class Config:
 
     def __init__(self):
         self.data = []
-        ConfigPath = str(Path(__file__).parent.parent / "Config.yaml")
+        ConfigPath = str(Path(__file__).absolute().parent.parent / "Config.yaml")
         with open(ConfigPath, "r") as stream:
             try:
                 self.data = yaml.safe_load(stream)
@@ -24,35 +24,52 @@ class Config:
         return self.data
 
     def GetScriptWorkspaceDir(self):
-        return self.data['ScriptWorkspaceDir']
+        return Path(self.data['ScriptWorkspaceDir'])
 
+    # Required Dirs
     def GetScriptWorkspaceTempDir(self):
-        return str(Path(self.GetScriptWorkspaceDir()) / "Temp")
-
-    def GetScriptWorkspaceCacheDir(self):
-        return str(Path(self.GetScriptWorkspaceDir()) / "Cache")
-
-    def GetScriptWorkspaceRecoveryDir(self):
-        return str(Path(self.GetScriptWorkspaceDir()) / "Recovery")
+        return Path(self.GetScriptWorkspaceDir()) / "Temp"
 
     def GetScriptWorkspaceServerDir(self):
-        return str(Path(self.GetScriptWorkspaceDir()) / "Server")
+        return Path(self.GetScriptWorkspaceDir()) / "Server"
+
+    def GetScriptWorkspaceSavesDir(self):
+        return Path(self.GetScriptWorkspaceDir()) / "Saves"
+    # ~Required Dirs
+
+    def GetScriptWorkspaceRequiredDirs(self):
+        return [self.GetScriptWorkspaceTempDir(), self.GetScriptWorkspaceServerDir(), self.GetScriptWorkspaceSavesDir() ]
+
+    def GetDefaultServerSettingsPath(self):
+        return Path(config.GetScriptWorkspaceDir()) / 'server-settings.json'
+
+    def GetDefaultMapGenSettingsPath(self):
+        return Path(config.GetScriptWorkspaceDir()) / 'map-gen-settings.json'
+
+    def GetDefaultMapSettingsPath(self):
+        return Path(config.GetScriptWorkspaceDir()) / 'map-settings.json'
 
     def GetServerDataModsDir(self):
-        return str(Path(self.data.CurrentServerData['ServerDataDir']) / "Mods")
+        return Path(self.data.CurrentServerData['ServerDataDir']) / "Mods"
 
-    def GetFactorioExecutable(self):
-        return str(Path(self.GetScriptWorkspaceServerDir()) / 'bin' / 'x64' / 'factorio')
+    def GetFactorioExecutablePath(self):
+        return Path(self.GetScriptWorkspaceServerDir() / 'factorio' / 'bin' / 'x64' / 'factorio')
+
+    def GetFactorioSavesDir(self):
+        return Path(self.GetScriptWorkspaceServerDir() / 'factorio' / 'saves')
+
+    def GetCurrentSaveFilePath(self):
+        return Path(self.GetScriptWorkspaceSavesDir() / self.GetData()['SaveName'] / (self.GetData()['SaveName'] + '.zip'))
+
+def InitiateWorkspaceIfNotReady():
+    for i in config.GetScriptWorkspaceRequiredDirs():
+        if not os.path.exists(i):
+            InitiateWorkspace()
+            break
 
 
-def InitializeWorkspace():
-    RequiredPaths = [
-        config.GetScriptWorkspaceTempDir(),
-        config.GetScriptWorkspaceCacheDir(),
-        config.GetScriptWorkspaceRecoveryDir()
-    ]
-
-    for i in RequiredPaths:
+def InitiateWorkspace():
+    for i in config.GetScriptWorkspaceRequiredDirs():
         if os.path.exists(i) == False or os.path.isdir(i) == False:
             os.makedirs(i, exist_ok=True)
 
@@ -63,7 +80,7 @@ def DownloadServerData():
     DownloadURL = f"https://www.factorio.com/get-download/{ServerVersion}/headless/linux64"
     response = requests.get(DownloadURL, stream=True)
 
-    PackagePath = str(Path(config.GetScriptWorkspaceTempDir()) / "FactorioServer.tar.xz")
+    PackagePath = str(config.GetScriptWorkspaceTempDir() / "FactorioServer.tar.xz")
 
     open(PackagePath, 'wb').write(response.content)
 
@@ -71,14 +88,46 @@ def DownloadServerData():
 
 def ExtractServerData(ServerDataPackagePath):
     Tar = tarfile.open(ServerDataPackagePath)
-    Tar.extractall(config.GetScriptWorkspaceServerDir())
+    Tar.extractall(str(config.GetScriptWorkspaceServerDir()))
 
+
+def TryCreateDefaultServerFiles():
+    if not os.path.exists(config.GetDefaultServerSettingsPath()):
+        shutil.copy(f"{str(config.GetScriptWorkspaceServerDir() / 'factorio' / 'data' / 'server-settings.example.json') }", f"{str(config.GetDefaultServerSettingsPath())}")
+
+    if not os.path.exists(config.GetDefaultMapGenSettingsPath()):
+        shutil.copy(f"{str(config.GetScriptWorkspaceServerDir() / 'factorio' / 'data' / 'map-gen-settings.example.json') }", f"{str(config.GetDefaultMapGenSettingsPath())}")
+
+    if not os.path.exists(config.GetDefaultMapSettingsPath()):
+        shutil.copy(f"{str(config.GetScriptWorkspaceServerDir() / 'factorio' / 'data' / 'map-settings.example.json') }", f"{str(config.GetDefaultMapSettingsPath())}")
+
+def CreateSaveData():
+
+    ExecutablePath = config.GetFactorioExecutablePath()
+    SaveName = config.GetData()['SaveName']
+    Command = f"{str(ExecutablePath)} --create { config.GetCurrentSaveFilePath() } --map-gen-settings {str(config.GetDefaultMapGenSettingsPath())} --map-settings {str(config.GetDefaultMapSettingsPath())} "
+
+    try:
+        p = subprocess.Popen(Command.split()).wait()
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError("command '{}' returned with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+
+    print("Factorio Server ended...")
+    return p
 
 def StartServer():
     print("Factorio Server started...")
 
-    ExecutablePath = config.GetFactorioExecutable()
-    Command = f"{ExecutablePath} --start-server {config.GetData()['SaveName']}"
+    ExecutablePath = config.GetFactorioExecutablePath()
+    Command = f"{str(ExecutablePath)} "
+
+    # Todo : Load save files apropriate to the server.
+    if not os.path.exists(config.GetFactorioSavesDir()):
+        Command += f"--start-server {config.GetCurrentSaveFilePath()} "
+    else:
+        Command += f"--start-server-load-latest "
+
+    Command += f"--server-settings {str(config.GetDefaultServerSettingsPath())}"
 
     try:
         p = subprocess.Popen(Command.split()).wait()
@@ -90,8 +139,11 @@ def StartServer():
 
 
 def CleanUp():
-    shutil.rmtree(config.GetScriptWorkspaceTempDir())
+    shutil.rmtree(str(config.GetScriptWorkspaceTempDir()))
 
+
+def Purge():
+    shutil.rmtree(str(config.GetScriptWorkspaceDir()),  ignore_errors=True)
 
 if __name__ == '__main__':
     config = Config()
@@ -101,13 +153,18 @@ if __name__ == '__main__':
     else:
         arg1 = sys.argv[1]
 
-        if arg1 == "init":
-            InitializeWorkspace()
-        elif arg1 == "install":
+        if arg1 == "install":
+            InitiateWorkspaceIfNotReady()
             PackagePath = DownloadServerData()
             ExtractServerData(PackagePath)
             CleanUp()
+        elif arg1 == "init.data":
+            TryCreateDefaultServerFiles()
+        elif arg1 == "init.save":
+            CreateSaveData()
         elif arg1 == "start":
             StartServer()
+        elif arg1 == "purge":
+            Purge()
         elif arg1 == "cleanup":
             CleanUp()
